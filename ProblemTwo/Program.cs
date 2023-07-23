4160b93c25a2820f0afbffc5157a091a54eebd96
erickGoJi <erickgomezlol96@gmail.com>
2023-07-23 15:44:21 -0600

    change text

--- ProblemTwo/Program.cs
+++ ProblemTwo/Program.cs
@@ -4,5 +4,5 @@ Solution solution = new Solution();
 int[] nums = {2,7,11,15};
 int target = 9;
 var result1 = solution.TwoSum(nums, target);
-Console.WriteLine("Example 2:");
+Console.WriteLine("Exercise 2:");
 Console.WriteLine(string.Join(", ", result1));
